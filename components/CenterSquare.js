44f6fba58536e528d2418cbc8e8bd9b1d72917e6
arunachaleshwaran e <arunachaleshwaran@elpaviation.com>
2022-07-02 23:31:40 +0530

    ✨ added for Centring a squre box all the time. used in full screen text message and board

---
+++ components/CenterSquare.js
@@ -0,0 +1,15 @@
+import { AspectRatio, Box, Container, Flex } from 'native-base'
+import React from 'react'
+import { useWindowDimensions } from 'react-native';
+
+export default function CenterSquare({ children }) {
+	const { width, height } = useWindowDimensions();
+	return (
+		<Flex height={'full'} width={'full'} direction={'row'} grow={1} shrink={1} justifyContent={'center'} alignItems={'center'}>
+			{height > width && <AspectRatio width={'full'} ratio={{ base: 1 / 1 }}>
+				{children}
+			</AspectRatio>}
+			{width >= height && <AspectRatio height={'full'} ratio={{ base: 1 / 1 }}>{children}</AspectRatio>}
+		</Flex>
+	)
+}
