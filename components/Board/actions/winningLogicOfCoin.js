b2c40407d0242129a207f98d0660c24b662c62c6
arunachaleshwaran <arunachaleshwaran12168@gmail.com>
2022-07-11 22:46:14 +0530

    🐛 some mistakes in node model

--- components/Board/actions/winningLogicOfCoin.js
+++ components/Board/actions/winningLogicOfCoin.js
@@ -1,10 +1,10 @@
 import { findEleIndex } from ".";
 
+const isCoinPresent = (coins, key) => {
+	const i = coins.filter(i => i.node.key === key).length;
+	return i === 0 ? false : true;
+}
 export const winningLogicOfCoin = (coins, key, isInitial, nodes) => {
-	const isCoinPresent = (coins, key) => {
-		const i = coins.filter(i => i.node.key === key).length;
-		return i === 0 ? false : true;
-	}
 	const coin = isInitial ? { key, node: nodes[findEleIndex(nodes, key)] } : findEleIndex(coins, key);
 	const logics = coin.node.winningLogic;
 	if ((isCoinPresent(coins, coin.node[logics[0][0]].vertex.key) && isCoinPresent(coins, coin.node[logics[0][1]].vertex.key))
