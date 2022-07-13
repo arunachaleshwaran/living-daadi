0c7daa64280af2ee322269ad655c984f592f765d
arunachaleshwaran <arunachaleshwaran12168@gmail.com>
2022-07-13 15:06:09 +0530

    🔨 change to returnIndex option from return key

--- components/Board/actions/findEle.js
+++ components/Board/actions/findEle.js
@@ -1,7 +1,7 @@
 export const findEle = (arr, key, option) => {
-	option = { returnKey: false, raiseError: true, ...option }
-	const val = option.returnKey ? arr.findIndex(ele => ele.key === key) : arr.find(ele => ele.key === key);
-	if (option.returnKey ? val === -1 : !val) {
+	option = { returnIndex: false, raiseError: true, ...option }
+	const val = option.returnIndex ? arr.findIndex(ele => ele.key === key) : arr.find(ele => ele.key === key);
+	if (option.returnIndex ? val === -1 : !val) {
 		if (option.raiseError) throw new Error(`Element with key ${key} not found in array`);
 		return null;
 	}
